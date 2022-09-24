49efafbf440fb167b5ab26d8e0ad3de10b2e7143
Sebastian Gilberto <sgilberto@frba.utn.edu.ar>
2022-09-24 10:57:49 -0300

    Agrego archivos sql, docker y DER

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # utn-gdd-2c2022-tp
+
 Trabajo Práctico para el 2do Cuatrimestre de 2022, para la materia Gestión de Datos en UTN
+
+[DER en LucidChart](https://lucid.app/lucidchart/4b07c250-a3f0-4099-98a6-95581a7dbb32/edit?invitationId=inv_05041888-b584-4db2-a2a2-e8211068b1a5&page=vlwS8_zE-Bex#)
---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+docker compose down
+
+docker compose up -d
+
+echo "waiting for sql server..."
+
+while ! docker-compose exec gdd-practica /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Gdd2022!' -Q "SELECT @@VERSION"; do sleep 1; done
+
+echo "starting database bootstrap"
+
+docker exec -it gdd-gdd-practica-1 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Gdd2022!' -Q 'CREATE DATABASE GD2C2022'
+
+docker exec -it /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'Gdd2022!' -i gd_esquema.Schema.sql -a 32767 -o resultado_esquema_output.txt
+docker exec -it /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'Gdd2022!' -i gd_esquema.Maestra.sql -a 32767 -o resultado_table_output.txt
+docker exec -it /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'Gdd2022!' -i gd_esquema.Maestra.Table.sql -a 32767 -o resultado_datos_output.txt
+
+docker exec -it gdd-gdd-practica-1 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Gdd2022!' -Q "SELECT count(*) from GD2C2022.gd_esquema.Maestra"
+
+# Esto no va para el TP es para lo de practica
+
+DATA=$(sudo docker exec -it gdd-gdd-practica-1 /opt/mssql-tools/bin/sqlcmd -S localhost \
+    -U SA -P 'Gdd2022!' \
+    -Q 'RESTORE FILELISTONLY FROM DISK = "/var/opt/mssql/backup/GD2015C1.bak"' |
+    tr -s ' ' | cut -d " " -f 1 | sed '3q;d')
+
+LOG=$(sudo docker exec -it gdd-gdd-practica-1 /opt/mssql-tools/bin/sqlcmd -S localhost \
+    -U SA -P 'Gdd2022!' \
+    -Q 'RESTORE FILELISTONLY FROM DISK = "/var/opt/mssql/backup/GD2015C1.bak"' |
+    tr -s ' ' | cut -d " " -f 1 | sed '4q;d')
+
+echo "Data file name: $DATA"
+echo "Log file name: $LOG"
+
+docker exec -it gdd-gdd-practica-1 /opt/mssql-tools/bin/sqlcmd -S localhost \
+    -U SA -P 'Gdd2022!' \
+    -Q 'RESTORE DATABASE GD2C2022 FROM DISK=N'/var/opt/mssql/backup/GD2015C1.bak' WITH MOVE '$DATA' to '/var/opt/mssql/data/GESTION2022.mdf', MOVE '$LOG' to '/var/opt/mssql/data/GESTION2022_log.ldf', REPLACE, NOUNLOAD, STATS=10'
