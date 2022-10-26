a45907aa8bd9b25db3e696fc99b1c4e00b77cd22
Sebastian Gilberto <sgilberto@frba.utn.edu.ar>
2022-10-25 23:36:19 -0300

    Agrego alter tables

--- tp/Scripts/script_creacion_inicial.sql
+++ tp/Scripts/script_creacion_inicial.sql
@@ -86,18 +86,18 @@ CREATE TABLE GAME_OF_JOINS.ventas
   ( 
      venta_codigo DECIMAL(19,0) PRIMARY KEY, 
      venta_fecha    DATETIME2, 
-     cliente_dni     DECIMAL(18,0), 
-     id_canal_venta       INT, 
+     cliente_dni     DECIMAL(18,0), --fk
+     id_canal_venta       INT,  --fk    
      venta_total     DECIMAL(18,2), 
-     id_venta_medio_pago   INT, 
-	 id_venta_medio_envio		   INT,
+     id_venta_medio_pago   INT,  --fk
+	 id_venta_medio_envio		   INT, --fk
   )  
 
 CREATE TABLE GAME_OF_JOINS.ventas_medio_pago 
   ( 
      id       INT PRIMARY KEY, 
-     venta_medio_pago_costo     DECIMAL(18,2), 
-     id_venta_medio_pago    nvarchar(255), 
+     venta_medio_pago_costo     DECIMAL(18,2),
+     id_medio_pago    INT, --fk
   ) 
 
 CREATE TABLE GAME_OF_JOINS.medios_pago 
@@ -111,25 +111,25 @@ CREATE TABLE GAME_OF_JOINS.medios_pago
 CREATE TABLE GAME_OF_JOINS.ventas_descuento 
   ( 
      id     INT PRIMARY KEY, 
-     venta_codigo  DECIMAL(19,0), 
+     venta_codigo  DECIMAL(19,0), --fk
      venta_descuento_importe   DECIMAL(18,2),
-	 id_descuento   INT,
+	 id_descuento   INT, --fk
   ) 
 
 CREATE TABLE GAME_OF_JOINS.ventas_canales 
   ( 
      id     INT PRIMARY KEY, 
-     venta_codigo  DECIMAL(19,0), 
-     id_canal  INT, 
+     venta_codigo  DECIMAL(19,0), --fk
+     id_canal  INT,  --fk
      venta_canal_costo DECIMAL(18,2), 
 
   ) 
 
 CREATE TABLE GAME_OF_JOINS.ventas_cupones 
   ( 
-     venta_codigo DECIMAL(19,0) PRIMARY KEY,
-     venta_cupon_codigo     nvarchar(255), 
-     venta_cupon_importe   DECIMAL(18,2), 
+     venta_codigo DECIMAL(19,0) NOT NULL, --fk
+     venta_cupon_codigo     nvarchar(255) NOT NULL,   --fk
+     venta_cupon_importe   DECIMAL(18,2),
   ) 
 
 CREATE TABLE GAME_OF_JOINS.clientes 
@@ -141,16 +141,16 @@ CREATE TABLE GAME_OF_JOINS.clientes
      cliente_telefono       DECIMAL(18,0), 
      cliente_mail       nvarchar(255), 
      cliente_fecha_nac       DATETIME2, 
-     cliente_codigo_postal       DECIMAL(18,0), 
+     cliente_codigo_postal       DECIMAL(18,0),  --fk
   ) 
 
 CREATE TABLE GAME_OF_JOINS.cupones 
   ( 
-     venta_cupon_codigo      INT PRIMARY KEY, 
+     venta_cupon_codigo      nvarchar(255) PRIMARY KEY, 
      venta_cupon_fecha_desde         DATETIME2, 
      venta_cupon_fecha_hasta         DATETIME2, 
      venta_cupon_valor        DECIMAL(18,2), 
-     id_venta_cupon_tipo INT, 
+     id_venta_cupon_tipo INT,  --fk
   ) 
 
 CREATE TABLE GAME_OF_JOINS.canales
@@ -182,16 +182,16 @@ CREATE TABLE GAME_OF_JOINS.tipos_variantes_productos
 CREATE TABLE GAME_OF_JOINS.ventas_envios
   ( 
      id      INT PRIMARY KEY, 
-     venta_codigo DECIMAL(19,0), 
+     venta_codigo DECIMAL(19,0),  --fk
      venta_envio_precio    DECIMAL(18,2), 
-     id_medio_habilitado      nvarchar(255), 
+     id_medio_habilitado      INT,  --fk
   ) 
 
 CREATE TABLE GAME_OF_JOINS.medios_envios_habilitados
   ( 
      id      INT PRIMARY KEY, 
-     venta_medio_envio DECIMAL(18,0), 
-     codigo_postal    DECIMAL(18,2), 
+     venta_medio_envio DECIMAL(18,0),  --fk
+     codigo_postal    DECIMAL(18,0),  --fk
      venta_envio_precio_actual      DECIMAL(18,2), 
 	 tiempo_estimado_envio		   DECIMAL(19,0)
   ) 
@@ -199,14 +199,14 @@ CREATE TABLE GAME_OF_JOINS.medios_envios_habilitados
 CREATE TABLE GAME_OF_JOINS.codigos_postales
   ( 
      codigo_postal      DECIMAL(18,0) PRIMARY KEY, 
-     id_localidad INT, 
+     id_localidad INT,  --fk
   ) 
 
 CREATE TABLE GAME_OF_JOINS.localidades
   ( 
      id      INT PRIMARY KEY, 
      localidad nvarchar(255), 
-     id_provincia    INT, 
+     id_provincia    INT,  --fk
   ) 
 
 CREATE TABLE GAME_OF_JOINS.provincias
@@ -241,9 +241,9 @@ CREATE TABLE GAME_OF_JOINS.categorias_productos
 CREATE TABLE GAME_OF_JOINS.productos_ventas 
   ( 
 	 id INT PRIMARY KEY,
-     producto_codigo    nvarchar(50), 
-	 venta_codigo   DECIMAL(19,0),
-     producto_variante_codigo    nvarchar(50), 
+     producto_codigo    nvarchar(50),  --fk
+	 venta_codigo   DECIMAL(19,0), --fk
+     producto_variante_codigo    nvarchar(50),  --fk
      venta_producto_cantidad    DECIMAL(18,0), 
      venta_producto_precio    DECIMAL(18,2), 
      venta_producto_total    DECIMAL(18,2), 
@@ -252,17 +252,17 @@ CREATE TABLE GAME_OF_JOINS.productos_ventas
 CREATE TABLE GAME_OF_JOINS.variantes_productos 
   ( 
 	 producto_variante_codigo nvarchar(50) PRIMARY KEY,
-     producto_codigo    nvarchar(50), 
-	 id_tipo_variante_producto   INT,
+     producto_codigo    nvarchar(50),  --fk
+	 id_tipo_variante_producto   INT, --fk
      variante_producto_precio    DECIMAL(18,2), 
   ) 
 
 CREATE TABLE GAME_OF_JOINS.productos_compras
   ( 
 	 id INT PRIMARY KEY,
-     producto_codigo    nvarchar(50), 
-	 compra_numero   DECIMAL(19,0),
-     producto_variante_codigo    nvarchar(50), 
+     producto_codigo    nvarchar(50),  --fk
+	 compra_numero   DECIMAL(19,0), --fk
+     producto_variante_codigo    nvarchar(50),  --fk
      compra_producto_cantidad    DECIMAL(18,2), 
      compra_producto_precio    DECIMAL(18,2), 
      compra_total    DECIMAL(18,2), 
@@ -271,7 +271,7 @@ CREATE TABLE GAME_OF_JOINS.productos_compras
 CREATE TABLE GAME_OF_JOINS.compras_descuentos 
   ( 
 	 id INT PRIMARY KEY,
-     compra_numero    DECIMAL(19,0), 
+     compra_numero    DECIMAL(19,0),  --fk
 	 descuento_compra_valor   DECIMAL(18,2),
      descuento_compra_codigo    DECIMAL(19,0), 
   ) 
@@ -280,8 +280,8 @@ CREATE TABLE GAME_OF_JOINS.compras
   ( 
 	 compra_numero DECIMAL(19,0) PRIMARY KEY,
      compra_fecha    DATETIME2, 
-	 proveedor_cuit   DECIMAL(19,0),
-     id_compra_medio_pago   INT,
+	 proveedor_cuit   nvarchar(50), --fk
+     id_compra_medio_pago   INT, --fk
      compra_total    DECIMAL(18,2), 
   ) 
 
@@ -296,9 +296,9 @@ CREATE TABLE GAME_OF_JOINS.productos
 	 producto_codigo nvarchar(50) PRIMARY KEY,
      producto_nombre    nvarchar(50), 
 	 producto_descripcion   nvarchar(50),
-     id_producto_categoria    INT, 
-     id_producto_marca    INT, 
-     id_producto_material    INT, 
+     id_producto_categoria    INT,  --fk
+     id_producto_marca    INT,  --fk
+     id_producto_material    INT,  --fk
   ) 
 
 CREATE TABLE GAME_OF_JOINS.proveedores 
@@ -307,5 +307,166 @@ CREATE TABLE GAME_OF_JOINS.proveedores
      proveedor_razon_social   nvarchar(50), 
 	 proveedor_domicilio   nvarchar(50),
      proveedor_mail    nvarchar(50), 
-     proveedor_codigo_postal    DECIMAL(18,0), 
+     proveedor_codigo_postal    DECIMAL(18,0),  --fk
   ) 
+
+-- Regla para nombrar FKs: FK_tabla_origen_nombre_campo 
+
+--ventas 
+ALTER TABLE GAME_OF_JOINS.ventas 
+  ADD CONSTRAINT fk_ventas_cliente_dni FOREIGN KEY (cliente_dni) REFERENCES GAME_OF_JOINS.clientes(cliente_dni) 
+
+ALTER TABLE GAME_OF_JOINS.ventas 
+  ADD CONSTRAINT fk_ventas_id_canal_venta FOREIGN KEY (id_canal_venta) REFERENCES GAME_OF_JOINS.ventas_canales(id) 
+
+ALTER TABLE GAME_OF_JOINS.ventas 
+  ADD CONSTRAINT fk_ventas_id_venta_medio_pago FOREIGN KEY (id_venta_medio_pago) REFERENCES GAME_OF_JOINS.ventas_medio_pago(id) 
+
+ALTER TABLE GAME_OF_JOINS.ventas 
+  ADD CONSTRAINT fk_ventas_id_venta_medio_envio FOREIGN KEY (id_venta_medio_envio) REFERENCES GAME_OF_JOINS.ventas_envios(id) 
+
+GO
+
+--ventas_medio_pago 
+ALTER TABLE GAME_OF_JOINS.ventas_medio_pago 
+  ADD CONSTRAINT fk_ventas_medio_pago_id_medio_pago FOREIGN KEY (id_medio_pago) REFERENCES GAME_OF_JOINS.medios_pago(id)
+
+GO
+
+--ventas_descuento 
+ALTER TABLE GAME_OF_JOINS.ventas_descuento 
+  ADD CONSTRAINT fk_ventas_descuento_venta_codigo FOREIGN KEY (venta_codigo) REFERENCES GAME_OF_JOINS.ventas(venta_codigo)
+
+ALTER TABLE GAME_OF_JOINS.ventas_descuento 
+  ADD CONSTRAINT fk_ventas_descuento_id_descuento FOREIGN KEY (id_descuento) REFERENCES GAME_OF_JOINS.descuentos(id)
+
+GO
+
+--ventas_canales 
+ALTER TABLE GAME_OF_JOINS.ventas_canales 
+  ADD CONSTRAINT fk_venta_canales_venta_codigo FOREIGN KEY (venta_codigo) REFERENCES GAME_OF_JOINS.ventas(venta_codigo)
+
+ALTER TABLE GAME_OF_JOINS.ventas_canales 
+  ADD CONSTRAINT fk_venta_canales_id_canal FOREIGN KEY (id_canal) REFERENCES GAME_OF_JOINS.canales(id)
+
+GO
+
+--ventas_cupones 
+ALTER TABLE GAME_OF_JOINS.ventas_cupones 
+  ADD CONSTRAINT pk_ventas_cupones  PRIMARY KEY (venta_codigo, venta_cupon_codigo)
+
+ALTER TABLE GAME_OF_JOINS.ventas_cupones 
+  ADD CONSTRAINT fk_ventas_cupones_venta_codigo FOREIGN KEY (venta_codigo) REFERENCES GAME_OF_JOINS.ventas(venta_codigo)
+
+ALTER TABLE GAME_OF_JOINS.ventas_cupones 
+  ADD CONSTRAINT fk_ventas_cupones_venta_cupon_codigo FOREIGN KEY (venta_cupon_codigo) REFERENCES GAME_OF_JOINS.cupones(venta_cupon_codigo)
+
+GO
+
+--clientes 
+ALTER TABLE GAME_OF_JOINS.clientes 
+  ADD CONSTRAINT fk_clientes_cliente_codigo_postal FOREIGN KEY (cliente_codigo_postal) REFERENCES GAME_OF_JOINS.codigos_postales(codigo_postal) 
+
+GO
+
+--cupones
+ALTER TABLE GAME_OF_JOINS.cupones 
+  ADD CONSTRAINT fk_cupones_id_venta_cupon_tipo FOREIGN KEY (id_venta_cupon_tipo) REFERENCES GAME_OF_JOINS.tipos_cupones(id)
+
+GO
+
+--ventas_envios
+ALTER TABLE GAME_OF_JOINS.ventas_envios 
+  ADD CONSTRAINT fk_ventas_envios_venta_codigo FOREIGN KEY (venta_codigo) REFERENCES GAME_OF_JOINS.ventas(venta_codigo) 
+
+ALTER TABLE GAME_OF_JOINS.ventas_envios
+  ADD CONSTRAINT fk_ventas_envios_id_medio_habilitado FOREIGN KEY (id_medio_habilitado) REFERENCES GAME_OF_JOINS.medios_envios_habilitados(id)
+
+GO
+
+--medios_envios_habilitados
+ALTER TABLE GAME_OF_JOINS.medios_envios_habilitados 
+  ADD CONSTRAINT fk_medios_envios_habilitados_venta_medio_envio FOREIGN KEY (venta_medio_envio) REFERENCES GAME_OF_JOINS.ventas_medios_envios(venta_medio_envio)
+
+ALTER TABLE GAME_OF_JOINS.medios_envios_habilitados
+  ADD CONSTRAINT fk_medios_envios_habilitados_codigo_postal FOREIGN KEY (codigo_postal) REFERENCES GAME_OF_JOINS.codigos_postales(codigo_postal)
+
+GO
+
+--codigos_postales
+ALTER TABLE GAME_OF_JOINS.codigos_postales 
+  ADD CONSTRAINT fk_codigos_postales_id_localidad FOREIGN KEY (id_localidad) REFERENCES GAME_OF_JOINS.localidades(id) 
+
+GO
+
+--localidades
+ALTER TABLE GAME_OF_JOINS.localidades 
+  ADD CONSTRAINT fk_localidades_id_provincia FOREIGN KEY (id_provincia) REFERENCES GAME_OF_JOINS.provincias(id) 
+
+GO
+
+--productos_ventas
+ALTER TABLE GAME_OF_JOINS.productos_ventas 
+  ADD CONSTRAINT fk_productos_ventas_producto_codigo FOREIGN KEY (producto_codigo) REFERENCES GAME_OF_JOINS.productos(producto_codigo) 
+
+ALTER TABLE GAME_OF_JOINS.productos_ventas 
+  ADD CONSTRAINT fk_productos_ventas_venta_codigo FOREIGN KEY (venta_codigo) REFERENCES GAME_OF_JOINS.ventas(venta_codigo) 
+
+ALTER TABLE GAME_OF_JOINS.productos_ventas 
+  ADD CONSTRAINT fk_productos_ventas_producto_variante_codigo FOREIGN KEY (producto_variante_codigo) REFERENCES GAME_OF_JOINS.variantes_productos(producto_variante_codigo) 
+
+GO
+
+--variantes_productos
+ALTER TABLE GAME_OF_JOINS.variantes_productos 
+  ADD CONSTRAINT fk_variantes_productos_producto_codigo FOREIGN KEY (producto_codigo) REFERENCES GAME_OF_JOINS.productos(producto_codigo) 
+
+ALTER TABLE GAME_OF_JOINS.variantes_productos 
+  ADD CONSTRAINT fk_variantes_productos_id_tipo_variante_producto FOREIGN KEY (id_tipo_variante_producto) REFERENCES GAME_OF_JOINS.tipos_variantes_productos(id) 
+
+GO
+
+--productos_compras
+ALTER TABLE GAME_OF_JOINS.productos_compras 
+  ADD CONSTRAINT fk_productos_compras_producto_codigo FOREIGN KEY (producto_codigo) REFERENCES GAME_OF_JOINS.productos(producto_codigo) 
+
+ALTER TABLE GAME_OF_JOINS.productos_compras 
+  ADD CONSTRAINT fk_productos_compras_compra_numero FOREIGN KEY (compra_numero) REFERENCES GAME_OF_JOINS.compras(compra_numero) 
+
+ALTER TABLE GAME_OF_JOINS.productos_compras 
+  ADD CONSTRAINT fk_productos_compras_producto_variante_codigo FOREIGN KEY (producto_variante_codigo) REFERENCES GAME_OF_JOINS.variantes_productos(producto_variante_codigo) 
+
+GO
+
+--compras_descuentos
+ALTER TABLE GAME_OF_JOINS.compras_descuentos 
+  ADD CONSTRAINT fk_compras_descuentos_compra_numero FOREIGN KEY (compra_numero) REFERENCES GAME_OF_JOINS.compras(compra_numero) 
+
+GO
+
+--compras
+ALTER TABLE GAME_OF_JOINS.compras 
+  ADD CONSTRAINT fk_compras_proveedor_cuit FOREIGN KEY (proveedor_cuit) REFERENCES GAME_OF_JOINS.proveedores(proveedor_cuit) 
+
+ALTER TABLE GAME_OF_JOINS.compras 
+  ADD CONSTRAINT fk_compras_id_compra_medio_pago FOREIGN KEY (id_compra_medio_pago) REFERENCES GAME_OF_JOINS.compras_medio_pago(id) 
+
+GO
+
+--productos
+ALTER TABLE GAME_OF_JOINS.productos 
+  ADD CONSTRAINT fk_productos_id_producto_categoria FOREIGN KEY (id_producto_categoria) REFERENCES GAME_OF_JOINS.categorias_productos(id) 
+
+ALTER TABLE GAME_OF_JOINS.productos 
+  ADD CONSTRAINT fk_productos_id_producto_marca FOREIGN KEY (id_producto_marca) REFERENCES GAME_OF_JOINS.productos_marcas(id) 
+
+ALTER TABLE GAME_OF_JOINS.productos 
+  ADD CONSTRAINT fk_productos_id_producto_material FOREIGN KEY (id_producto_material) REFERENCES GAME_OF_JOINS.productos_material(id) 
+
+GO
+
+--proveedores
+ALTER TABLE GAME_OF_JOINS.proveedores 
+  ADD CONSTRAINT fk_proveedores_proveedor_codigo_postal FOREIGN KEY (proveedor_codigo_postal) REFERENCES GAME_OF_JOINS.codigos_postales(codigo_postal) 
+
+GO
